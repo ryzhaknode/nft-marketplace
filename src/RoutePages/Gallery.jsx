64aae4a2c17ac67e91b686a8c810155130e9dfbc
ryzhaknode <denryzhak@gmail.com>
2023-07-03 14:38:59 +0300

    create nft-cards

--- src/RoutePages/Gallery.jsx
+++ src/RoutePages/Gallery.jsx
@@ -1,9 +1,42 @@
-import React, { useEffect, useState } from "react";
+import data from "../nftsItems.json";
+import { Box, Card, CardActionArea } from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
 function Gallery() {
   return (
-    <div>
-      <h1>Gallery</h1>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "20px",
+        flexWrap: "wrap",
+        m: 2,
+      }}
+    >
+      {data.map((card, i) => (
+        <Card key={i} sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="300"
+              image={card.images[0].url}
+              alt={card.images[0].name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {card.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {card.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Box>
   );
 }
 
