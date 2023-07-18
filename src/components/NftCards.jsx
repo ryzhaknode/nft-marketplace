2fa23181613d00f8cf5637fdc3e52ddf599555bc
ryzhaknode <denryzhak@gmail.com>
2023-07-18 20:46:15 +0300

    add new styles in NftCards

--- src/components/NftCards.jsx
+++ src/components/NftCards.jsx
@@ -1,25 +1,57 @@
-import { Card, CardActionArea } from "@mui/material";
+import { Card, CardActionArea, Box } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
 function NftCards({ card }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+    <Card sx={{ width: 345, minHeight: 475 }}>
+      <CardActionArea sx={{ padding: "20px" }}>
         <CardMedia
           component="img"
           height="300"
           image={card.images[0].url}
           alt={card.images[0].name}
+          sx={{ borderRadius: "5%" }}
         />
-        <CardContent>
-          <Typography variant="h5" component="div">
+        <CardContent sx={{ minHeight: 75 }}>
+          <Typography variant="h6" component="div">
             {card.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {card.description}
+            {card.description.split(" ").length > 8
+              ? `${card.description.split(" ").slice(0, 8).join(" ")}...`
+              : card.description}
           </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              sx={{ paddingTop: "10px" }}
+              variant="h6"
+              component="div"
+            >
+              {`${card.price} ETH`}
+            </Typography>
+
+            <Typography
+              sx={{
+                padding: "5px 10px",
+                backgroundColor: "rgba(215, 112, 229, 0.2)",
+                borderRadius: "10px",
+                color: "#D770E5",
+                textTransform: "lowercase",
+              }}
+              variant="body2"
+              component="div"
+            >
+              {card.interests[0].name ? card.interests[0].name : "none"}
+            </Typography>
+          </Box>
         </CardContent>
       </CardActionArea>
     </Card>
