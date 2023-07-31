9f0ade2b4aa8bd2315c38747673024f7a7278bb1
ryzhaknode <denryzhak@gmail.com>
2023-07-31 14:01:29 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 
+const cors = require("cors");
 const express = require("express");
-
+const sequelize = require("./db");
 const PORT = process.env.PORT || 5000;
-
+const models = require("./models/models");
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
---
+++ server/models/models.js
@@ -0,0 +1,64 @@
+const sequelize = require("../db");
+
+const { v4: uuidv4 } = require("uuid");
+
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+// const BasketNft = sequelize.define("basket_device ", {
+//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+// });
+
+const NftCard = sequelize.define("nft_card", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+  nftCodeNumber8: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: function () {
+      return uuidv4().replace(/-/g, "").slice(0, 8);
+    },
+    unique: true,
+  },
+
+  interests: {
+    type: DataTypes.ARRAY(DataTypes.JSON),
+    allowNull: false,
+  },
+
+  images: {
+    type: DataTypes.ARRAY(DataTypes.JSON),
+    allowNull: false,
+  },
+
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+  authorName: { type: DataTypes.STRING, allowNull: false },
+  companyName: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Rating = sequelize.define("rating", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  rate: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+User.hasMany(NftCard);
+NftCard.belongsTo(User);
+
+NftCard.hasOne(User);
+User.belongsTo(NftCard);
+
+NftCard.hasMany(Rating);
+Rating.belongsTo(NftCard);
+
+module.exports = { User, Rating, NftCard };
